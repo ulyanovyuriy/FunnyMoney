db6479550b3216109819b950efe06dc68528866e
ulyanov yuriy <ulyanov.yuriy@live.com>
2018-08-15 15:52:01 +0300

    add XMRN.Common proj

---
+++ src/XMRN.Common/Data/DataReaderExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace XMRN.Common.Data
+{
+    public static class DataReaderExtension
+    {
+        public static IEnumerable<IDataRecord> AsEnumerable(this IDataReader reader)
+        {
+            if (reader == null) throw new ArgumentNullException(nameof(reader));
+            while (reader.Read())
+                yield return reader;
+        }
+    }
+}
---
+++ src/XMRN.Common/Data/DataRecordExtension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XMRN.Common.Data
+{
+    public static class DataRecordExtension
+    {
+
+    }
+}
