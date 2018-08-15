c561ca9c7f73894d5cf29cb8f98e42ab47862ce8
ulyanov yuriy <ulyanov.yuriy@live.com>
2018-08-15 12:23:04 +0300

    Add XMRN.Android.Common proj

---
+++ src/XMRN.Android.Common/Cursor/CursorField.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XMRN.Android.Common.Cursor
+{
+    public class CursorField
+    {
+        public string Name { get; set; }
+
+        public CursorFieldType Type { get; set; }
+    }
+}
