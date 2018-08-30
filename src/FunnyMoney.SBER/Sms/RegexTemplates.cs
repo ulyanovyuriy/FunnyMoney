f9b58465c86c6f920ec7cc3e3bfe66cd393d9a3d
ulyanov yuriy <ulyanov.yuriy@live.com>
2018-08-30 15:29:17 +0300

    restruct

---
+++ src/FunnyMoney.SBER/Sms/RegexTemplates.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunnyMoney.SBER.Sms
+{
+    public static class RegexTemplates
+    {
+        public static string WriteOff =
+            @"(?<CN>VISA\d{4}) (?<DT>\d{2}\.\d{2}\.\d{2} \d{2}:\d{2}) списание (?<V>\d+(\.\d{2})?р)( с комиссией (?<C>\d+(\.\d{2})?р))?(\s+(?<T>.*))? Баланс: (?<B>\d+(\.\d{2})?р)";
+    }
+}
